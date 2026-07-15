949a30c701ee0799e619391f038f25a073a200aa
Jessica Meixner <Jessica.Meixner@noaa.gov>
2026-07-15 18:38:37 +0000

    update switch file

--- sorc/make.all.sh
+++ sorc/make.all.sh
@@ -43,7 +43,7 @@ done
 # 2. Build WW3 model code 
 # Determine which switch to use 
 
-ww3switch=model/bin/switch_NCEP_glwu
+ww3switch=model/bin/switch_NCEP_rwps
 
 
 # Check final exec folder exists
